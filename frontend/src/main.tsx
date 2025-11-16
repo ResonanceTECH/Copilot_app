98366eb6de81e3a639e2e8d89838a724c602e64f
Daria Chugunova <dariachugunova2003@gmail.com>
2025-11-16 23:59:36 +0300

    feat: добавлен LanguageProvider в точку входа приложения

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AssistantPage } from './pages/AssistantPage';
+import { LanguageProvider } from './contexts/LanguageContext';
 import './styles/variables.css';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AssistantPage />
+    <LanguageProvider>
+      <AssistantPage />
+    </LanguageProvider>
   </React.StrictMode>
 );
 
